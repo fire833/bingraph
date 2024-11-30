3196f541fbfb232078d4c11a5ed64eab41e8d76e
fire833 <kttpsy@gmail.com>
2024-11-30 13:48:09 -0600

    fix: update scripts to build correct log histograms

--- scripts/build_gallery.sh
+++ scripts/build_gallery.sh
@@ -18,8 +18,21 @@ podman run -it -v ./data:/data docker.io/amazonlinux:2023 ./data/bingraph -b /us
 # Debian bookworm
 podman run -it -v ./data:/data docker.io/debian:bookworm ./data/bingraph -b /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -o /data/debianbookworm.json --output-graphviz /data/debianbookworm.dot
 
+echo "building distribution plots..."
+./scripts/build_histogram data/amazonlinux2023.json data/amazonlinux2023.jpeg
+./scripts/build_histogram data/ubuntu2204.json data/ubuntu2204.jpeg
+./scripts/build_histogram data/ubuntu2410.json data/ubuntu2410.jpeg
+./scripts/build_histogram data/debianbookworm.json data/debianbookworm.jpeg
+./scripts/build_histogram data/arch.json data/arch.jpeg
+
+echo "building graphs..."
+echo "building al2023..."
 dot -Tsvg -Kfdp data/amazonlinux2023.dot -o data/amazonlinux2023.svg
+echo "building ubuntu 22.04..."
 dot -Tsvg -Kfdp data/ubuntu2204.dot -o data/ubuntu2204.svg
+echo "building ubuntu 24.10..."
 dot -Tsvg -Kfdp data/ubuntu2410.dot -o data/ubuntu2410.svg
+echo "building debian bookworm..."
 dot -Tsvg -Kfdp data/debianbookworm.dot -o data/debianbookworm.svg
+echo "building arch..."
 dot -Tsvg -Kfdp data/arch.dot -o data/arch.svg
